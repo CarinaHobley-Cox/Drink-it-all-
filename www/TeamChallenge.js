4a7eb5cd936fa4b6b895ea7edcf917d949853551
CarinaHobley-Cox <32267199+CarinaHobley-Cox@users.noreply.github.com>
2018-05-04 17:20:55 +0100

    Add files via upload

--- www/TeamChallenge.js
+++ www/TeamChallenge.js
@@ -21,37 +21,37 @@ $("#Statement4").text ("Name as many Marvel characters");
 
 else if(rnd==1){
 
-$("#Statement4").text ("Name as many Disney characters ");
+$("#Statement4").text ("Name as many Disney characters");
 
 }
  else if (rnd==2) {
 
-$("#Statement4").text ("Charades: Name the film: “Castaway”");
+$("#Statement4").text ("Charades: Name the film: “Castaway");
 
 }
      else if (rnd==3) {
 
-$("#Statement4").text ("Name the film: “ life is like a box of chocolates”");
+$("#Statement4").text ("Name the film: “ life is like a box of chocolates");
 
 }
         else if (rnd==4) {
 
-$("#Statement4").text ("Charades: Name the film: “Frozen”");
+$("#Statement4").text ("Charades: Name the film: “Frozen");
 
 }
         else if (rnd==5) {
 
-$("#Statement4").text ("Charades: Name the TV show: “Big bang theory”");
+$("#Statement4").text ("Charades: Name the TV show: “Big bang theory");
 
 }
         else if (rnd==6) {
 
-$("#Statement4").text ("Charades: Name the TV show: “Doctor who”");
+$("#Statement4").text ("Charades: Name the TV show: “Doctor who");
 
 }
            else if (rnd==7) {
 
-$("#Statement4").text ("Charades: Name the TV show: “Game of thrones”");
+$("#Statement4").text ("Charades: Name the TV show: “Game of thrones");
 
 }
            else if (rnd==8) {
